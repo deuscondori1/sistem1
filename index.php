24ed82922c33ea59745db9b10c8bd6efe7490744
deuscondori1 <jokercondori828@gmail.com>
2024-03-14 17:48:53 -0400

    se agregaron nuevos campos nonquery y query en el archivo conexion.php hay que arregra la linea 62

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
-require_once "src/conexion/conexion.php";
+    require_once "src/conexion/conexion.php";
+    
     $conexion = new conexion;
+
+    //$query = "select * from usuarios";
+    $query = "INSERT IBTO usuarios (DNI)value('1')";
+    
+    //print_r($conexion->obtenerDatos($query));
+
+    print_r($conexion->nonQuery($query));
 ?>
